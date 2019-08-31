11f3e8c18da3ead883e7e7ac7ad9d2b0ff030296
Tanmay-Goel <53892924+Tanmay-Goel@users.noreply.github.com>
2019-08-31 22:08:46 +0530

    Add files via upload

--- mergesort.cpp
+++ mergesort.cpp
@@ -2,78 +2,88 @@
 #include<chrono>
 using namespace std;
 using namespace std::chrono;
-
-void merge(int b[], int p, int c[], int q, int a[])
+void merge(int arr[], int low, int mid, int high)
 {
-	int i=0,j=0,k=0;
-	while(i<p && j<q)
-	{
-		if(b[i]<=c[i])
-		{
-			a[k]=b[i];
-			i++;
-		}
-		else
-		{
-			a[k]=c[j];
-			j++;
-		}
-		k++;
-	}
-	if(i==p)
-	{
-		for(int t=j;t<q;t++)
-		{
-			a[k]=c[t];
-			k++;
-		}
-	}
-	else if(j==q)
-	{
-		for(int t=i;t<p;t++)
-		{
-			a[k]=b[t];
-			k++;
-		}
-	}
+    int i, j, k;
+    int n1 = mid - low + 1;
+    int n2 =  high - mid;
+
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[low + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1+ j];
+
+    i = 0;
+    j = 0;
+    k = low;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void mergesort(int a[], int n)
+
+void mergeSort(int arr[], int low, int high)
 {
-	int x=n/2,y=n/2;
-	if(n%2==1)
-		y++;
-	int b[x], c[y],i,j;
-	if(n>1)
-	{
-		for(i=0;i<x;i++)
-			b[i]=a[i];
-		for(j=0;j<y;j++,i++)
-			c[j]=a[i];
-		mergesort(b,x);
-		mergesort(c,y);
-		merge(b,x,c,y,a);
-	}
+
+    if (low < high)
+    {
+        int mid = low+(high - low)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+		merge(arr, low, mid, high);
+    }
 }
+
+
 int main()
 {
-	int n, a[10],i;
-	cout<<"Enter size: ";
+	int arr[10],i,j,high,low,n;
+	cout<<"Enter no of elements:";
 	cin>>n;
-	cout<<"Enter array: ";
-	for(i=0;i<n;i++)
-		cin>>a[i];
-		
+	low=0;
+	high=n-1;
+	cout<<"Enter elements:";
+	for(i =0;i<n;i++)
+		cin>>arr[i];
 	auto start = high_resolution_clock::now();
-	mergesort(a,n);
+	mergeSort(arr, low, high);
 	auto stop = high_resolution_clock::now();
-	
+
 	cout<<"sorted array: ";
 	for(i=0;i<n;i++)
 		cout<<a[i]<<" ";
 	std::chrono::duration<double, std::milli> fp_ms = stop - start;
-	
-	cout<<"\ntime in milliseconds: "<<fp_ms.count()<<endl; 
-	
-	return 0;
- }
+
+	cout<<"\ntime in milliseconds: "<<fp_ms.count()<<endl;
+
+    return 0;
+}
